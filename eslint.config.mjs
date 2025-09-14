ba1f3b54b06fc3ea0896f4046ce52bf6af282b53
jatin <jatin1804sharma@gmail.com>
2025-09-14 16:38:46 +0530

    :hammer: (fix/build): Build config updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-empty-interface": ["error"],
+      "@typescript-eslint/no-explicit-any": ["error"],
+      "react/no-unescaped-entities": ["error"],
+    },
+  },
 ];
 
 export default eslintConfig;
